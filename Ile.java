1bd077079a221920f710bb21f394629c3ca75d48
Manda Andriamaromanana <aroniaina.andriam@gmail.com>
2022-03-21 17:31:03 +0100

    ajout des fichiers pour le projet

---
+++ Ile.java
@@ -0,0 +1,9 @@
+import IG.*;
+
+public class Ile {
+    public static void main(String[] args) {
+        int nb = 8;
+	    Fenetre fenetre = new Fenetre(nb + " reines");
+        fenetre.dessineFenetre();
+    }
+}
