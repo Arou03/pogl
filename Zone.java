83b605142c152954e68e135671c327ca78336894
nomenjanahary.rasoanaivo <nomenjanahary.rasoanaivo@paris-saclay.fr>
2022-03-23 13:31:50 +0100

    Peur de commit

---
+++ Zone.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Zone{
+    enum Etat{
+        NORMAL,
+        INONDEE,
+        SUBMERGEE,
+    }
+    // j'ai pas trop compris mais je l'ai quand même fait comme dans la classe ile 
+
+}
