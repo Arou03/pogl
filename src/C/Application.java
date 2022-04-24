798ebb0328ada7601f44557fa029e1f3d386f503
Manda Andriamaromanana <aroniaina.andriam@gmail.com>
2022-04-24 12:39:23 +0200

    J'ai réorganiser le main et renommé bouton

---
+++ src/C/Application.java
@@ -0,0 +1,97 @@
+package C;
+
+import java.awt.GridBagConstraints;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import javax.swing.JButton;
+
+import IG.Fenetre;
+import IG.Texte;
+import M.ileM;
+import V.IleV;
+
+public class Application {
+
+    public static void creerLaFenetre() {
+        Scanner entree = new Scanner(System.in);
+        int dimension = 10; 
+        int nbJoueur = 4;
+        System.out.println("Quelle sera la taille du plateau ? ");
+        try {
+            dimension = entree.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Entrée invalide.\n Taille par défaut : 10");
+        }
+        System.out.println("Combien de joueurs voulez vous ? ");
+        try {
+            nbJoueur = entree.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Entrée invalide.\n Nombre de joueur par défaut : 4");
+        }
+        entree.close();
+        JButton BoutonFdt = new JButton("Fin de tour");
+        JButton BoutonEnd = new JButton("Exit");
+        Texte AfficheTour = new Texte("");
+        Texte AfficheAction = new Texte("");
+        Fenetre fenetre = new Fenetre( "Ile Interdite");
+        ileM modele = new ileM(dimension, nbJoueur);
+        IleV Vue = new IleV(modele);
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.NONE;
+        c.weightx = 0.5;
+        c.weighty = 0.5;
+        c.gridheight = 10;
+        c.gridwidth = 10;
+        c.gridx = 0;
+	    c.gridy = 0;
+        fenetre.ajouteElement(Vue, c);
+        c.fill = GridBagConstraints.NONE;
+        c.gridheight = 1;
+        c.gridwidth = 1;
+	    c.gridx = 11;
+        c.gridy = 0;
+        AfficheTour.changeTexte("Tour du joueur " + (modele.tourEnCours + 1) );
+        fenetre.ajouteElement(AfficheTour, c);
+        c.gridy = 1;
+        AfficheAction.changeTexte("Nombre d'action restantes : " + (3 - modele.joueurs.get(modele.tourEnCours).nbAction) );
+        fenetre.ajouteElement(AfficheAction, c);
+        c.gridy = 2;
+        fenetre.ajouteElement(BoutonFdt, c);
+        c.gridy = 3;
+        fenetre.ajouteElement(BoutonEnd, c);
+        /**
+         * ajout des actions  des boutons 
+         */
+        BoutonFdt.addActionListener(e ->{
+            modele.innonde_random();
+            AfficheTour.changeTexte("Tour du joueur " + (modele.tourEnCours + 1) );
+            Vue.actualise();
+        });
+
+        BoutonEnd.addActionListener(e ->{
+            fenetre.dispose();
+        });
+
+        
+
+        fenetre.dessineFenetre();
+    }
+     
+    
+
+    /**
+     * constructeur Bouton 
+     * @param f
+     * @param ile
+     */
+
+    public Application (){
+
+    }
+
+    public void update(Texte AfficheTour, Texte AfficheAction, ileM ile) {
+        AfficheTour.changeTexte("Tour du joueur " + (ile.tourEnCours + 1) );
+        AfficheAction.changeTexte("Nombre d'action restantes : " + (3 - ile.joueurs.get(ile.tourEnCours).nbAction) );
+    }
+}
--- src/IleInterdite.java
+++ src/IleInterdite.java
@@ -1,34 +1,12 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
-import C.Bouton;
-import IG.Fenetre;
-import M.ileM;
+import C.Application;
 
 public class IleInterdite {
     public static void main(String[] args) {
-        int dimension = 10; 
-        int nbJoueur = 4;
-        Scanner entree = new Scanner(System.in);
-        System.out.println("Combien de joueurs voulez-vous ? ");
-        try {
-            nbJoueur = entree.nextInt();
-        } catch (InputMismatchException e) {
-            System.out.println("Entrée invalide.\n Nombre de joueur par défaut : 4");
-        }
-        entree.close();
-
         /**
          * création de la fenêtre 
          * et ajout de notre modèle 
          */
-        Fenetre fenetre = new Fenetre( "Ile Interdite");
-        ileM modele = new ileM(dimension, nbJoueur);
-        Bouton b = new Bouton(fenetre, modele);
-
-
-        
-        fenetre.dessineFenetre();
+        Application.creerLaFenetre();
     }
 
 }
